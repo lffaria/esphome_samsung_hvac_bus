2c96748f8aa781c33a50b6b04799ab01b72dd50d
Luiz Faria <lef@isep.ipp.pt>
2026-03-23 09:04:22 +0000

    Disable MQTT (step 2)

--- components/samsung_ac/debug_mqtt.h
+++ components/samsung_ac/debug_mqtt.h
@@ -1,4 +1,4 @@
-#pragma once
+/* #pragma once
 
 #include <string>
 
@@ -10,4 +10,16 @@ namespace esphome
         void debug_mqtt_connect(const std::string &host, const uint16_t port, const std::string &username, const std::string &password);
         bool debug_mqtt_publish(const std::string &topic, const std::string &payload);
     } // namespace samsung_ac
-} // namespace esphome
+} // namespace esphome */
+
+#pragma once
+#include <string>
+
+inline bool debug_mqtt_connected() { return false; }
+
+inline void debug_mqtt_connect(const std::string &, const uint16_t,
+                               const std::string &, const std::string &) {}
+
+inline bool debug_mqtt_publish(const std::string &, const std::string &) {
+    return false;
+}
